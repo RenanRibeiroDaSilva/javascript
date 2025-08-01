4d28284df66639385e723f5beb5b416ab8890d53
Renan Ribeiro <84098891+RenanRibeiroDaSilva@users.noreply.github.com>
2025-08-01 14:33:55 -0300

    Praticando JS

---
+++ Excercícios/Basico/EX010/somaValores.js
@@ -0,0 +1,11 @@
+
+
+// Recebe valores:
+let n1 = Number(prompt('Digite um valor: '))
+let n2 = Number(prompt('Digite mais um valor: '))
+
+// Calculos:
+let soma = n1 + n2
+
+// Mostra em alerta
+alert(`A soma entre ${n1} + ${n2} = ${soma}`)
---
+++ Excercícios/Basico/Ex011/ficha.js
@@ -0,0 +1,10 @@
+
+
+// Recebe dados do usuário:
+let nome = prompt('Qual seu nome: ')
+let anoAtual = new Date().getFullYear()
+let idade = anoAtual - Number(prompt('Qual o ano do seu nascimento: ')) 
+let cidMora = prompt('Em qual cidade mora atualmente: ')
+
+// Informa via alerta para o usuário:
+alert(`Seu nome é ${nome}, você tem ${idade} de idade e mora na cidade de ${cidMora}`)
