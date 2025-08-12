00ba9e4a56d1464ebb0037d75b9b73e09abbb153
Renan Ribeiro <84098891+RenanRibeiroDaSilva@users.noreply.github.com>
2025-08-12 11:22:36 -0300

    Repetições (Parte 1) - Curso JavaScript #13
    Aprendendo While e Do While

---
+++ Curso em Video.com/Aula13 - Laço de Repetição: While/exe01.js
@@ -0,0 +1,7 @@
+// Laço de repetição com validação no início.
+let c = 1
+
+while (c <= 5000){
+    console.log(`(${c}° passo)`)
+    c++
+}
---
+++ Curso em Video.com/Aula13 - Laço de Repetição: While/exe02.js
@@ -0,0 +1,8 @@
+// Laço de repetição com validação no final.
+
+let c = 1
+
+do {
+    console.log(`${c}° passo`)
+    c++ 
+} while (c <= 10)
