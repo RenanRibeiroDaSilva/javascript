d1f1287418769e77ab9e55ace98b64f18567f008
Renan Ribeiro <84098891+RenanRibeiroDaSilva@users.noreply.github.com>
2025-08-13 11:18:10 -0300

    Praticando JS

--- Excercícios/Laços/ex001/for001.js
+++ Excercícios/Laços/ex001/for001.js
@@ -1,3 +1,24 @@
-let n1 = document.getElementById('n1')
-let n2 = document.getElementById('n2')
+function contagem(){
+    // Captura os valores
+    const inicioStr = document.getElementById('inicio').value
+    const fimStr = document.getElementById('fim').value    
 
+    // Faz a validação
+    if (inicioStr.trim() === '' || fimStr.trim() === '') {
+        alert('Preencha ambos os campos!')
+        return
+    }
+
+    // Torna Int
+    const inicio = Number(inicioStr)
+    const fim = Number(fimStr)
+
+    let resultado = ''
+
+    for (let i = inicio; i <= fim; i++) {
+        resultado += (i === fim) ? i : `${i}, `
+    }
+
+    res.innerHTML = `Inicio: ${inicio} <br> Fim: ${fim}`
+    res2.textContent = `Saída ${resultado}`
+}
